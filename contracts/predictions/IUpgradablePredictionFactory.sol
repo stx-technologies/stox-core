286bdd1ab0e2bfbf82840176a64f6a5656f4ca16
TzahiStox <Ts13112017>
2018-03-19 18:26:42 +0200

    dev

--- contracts/predictions/IUpgradablePredictionFactory.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.18;
-import "../token/IERC20Token.sol";
-import "./PoolPredictionPrizeDistribution.sol";
-
-contract IUpgradablePredictionFactory {
-    function createPoolPrediction(address _oracle, uint _predictionEndTimeSeconds, uint _optionBuyingEndTimeSeconds, string _name, IERC20Token _stox, PoolPredictionPrizeDistribution _poolPredictionPrizeDistribution) public;
-    event PoolPredictionCreated(address indexed _creator, address indexed _newPrediction);
-}
