fc2286d149ff3dea82d751faacaf80d1ffa15734
TzahiStox <Ts13112017>
2018-03-21 16:43:40 +0200

    dev

--- contracts/oracles/IOracleFactoryImpl.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.18;
-
-/*
-    @title IOracleFactoryImpl contract - A interface contract for the oracles factory.
- */
-contract IOracleFactoryImpl {
-    function createOracle(address _owner, string _name) public returns(address); 
-}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 //var PredictionFactory   = artifacts.require("./events/PredictionFactory.sol");
-var OracleFactory       = artifacts.require("./oracles/OracleFactory.sol");
+//var OracleFactory       = artifacts.require("./oracles/OracleFactory.sol");
 
 module.exports = function(deployer) {
     //deployer.deploy(PredictionFactory);
     //deployer.deploy(UpgradablePrediction)
-    deployer.deploy(OracleFactory);
+    //deployer.deploy(OracleFactory);
 };
